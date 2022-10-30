853186f889c135d10c91742a86ad31d6dd66b5d7
RusakovNikki <rusakov.nikki@yandex.ru>
2022-10-30 16:11:28 +0300

    добавлен компонент NotFoundBlock

---
+++ src/components/NotFoundBlock.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NotFoundBlock = () => {
+  return (
+    <div className="notfound-container">
+      <p className="title rubik-regular">Ничего не найдено </p>
+      <span className="notfound-container__smile">🤧</span>
+    </div>
+  )
+}
+
+export default NotFoundBlock
--- src/components/ShowJobs/index.jsx
+++ src/components/ShowJobs/index.jsx
@@ -2,9 +2,11 @@ import React from "react"
 import { useEffect } from "react"
 import { useState } from "react"
 import useJobFilter from "../../hooks/jobItems"
+import NotFoundBlock from "../NotFoundBlock"
 
 const ShowJobs = ({ sortByType }) => {
   const URL = `https://api.hh.ru/vacancies`
+  const countElementsOnPage = 5
 
   const [page, setPage] = useState(1)
   const [items, isLoading, jobItems, skeletons] = useJobFilter(URL, sortByType)
@@ -12,13 +14,21 @@ const ShowJobs = ({ sortByType }) => {
   useEffect(() => {
     setPage(1)
   }, [sortByType])
-  const jobsFiltered = jobItems?.filter((item, index) => index < 5 * page)
 
-  const maxCountPage = jobsFiltered.length / 5
+  const jobsFiltered = jobItems?.filter(
+    (_, index) => index < countElementsOnPage * page
+  )
+
+  const chowJobsFiltred = jobsFiltered.length ? jobsFiltered : <NotFoundBlock />
+  const countPajeFilt = Boolean(
+    Math.floor(jobsFiltered.length / countElementsOnPage)
+  )
+  const maxCountPage = items.length / countElementsOnPage
+
   return (
     <section className="jobs-container">
-      {isLoading ? skeletons : jobsFiltered}
-      {page <= maxCountPage && (
+      {isLoading ? skeletons : chowJobsFiltred}
+      {page < maxCountPage && countPajeFilt && (
         <button className="button" onClick={() => setPage((prev) => ++prev)}>
           Смотреть еще...
         </button>
